d93827f06f50f9e481be62d8dd4f830f41d3312e
wikibootup <wikibootup@gmail.com>
2016-05-11 00:07:48 +0900

    first steps of FT for layout + styling

--- ch07/superlists/functional_tests/tests.py
+++ ch07/superlists/functional_tests/tests.py
@@ -95,3 +95,17 @@ class NewVisitorTest(LiveServerTestCase):  #1
 
         # She is invited to enter a to-do item straight away
         # [...rest of comments as before]
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing\n')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
