0798090b266025620d8c71a135858484c8102bc2
wikibootup <wikibootup@gmail.com>
2016-05-11 16:35:08 +0900

    test seperation need to fix server_url error

--- ch10/superlists/functional_tests/test_simple_list_creation.py
+++ ch10/superlists/functional_tests/test_simple_list_creation.py
@@ -8,7 +8,7 @@ class NewVisitorTest(FunctionalTest):  #1
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
